1768dc7ea7eb86c97851516fd5db7267ae962096
Sam Thomas <sft89@pm.me>
2022-02-25 13:21:45 +0000

    feat: update stylings

--- src/index.tsx
+++ src/index.tsx
@@ -13,12 +13,11 @@ import {
    AtomicSearchBoxQuerySuggestions,
    AtomicResultSectionVisual,
    AtomicFacet,
-   AtomicFacetManager,
 } from '@coveo/atomic-react'
 
 import './app.css'
 
-const MyResultTemplateFunction = () => {
+const ResultTemplate = () => {
   return <>
     <AtomicResultSectionVisual style={{width: 200, height: 200}}>
       <AtomicResultImage field="image" />
@@ -39,25 +38,23 @@ const App = () => {
       accessToken: process.env.APIKEY as string,
       organizationId: process.env.ORGID as string,
       search: {searchHub: 'default' },
-      
     }
   });
+
   return <div>
     <div className='thing'>
-      <img  src='https://user-images.githubusercontent.com/10165959/155548924-be99c42a-30c3-4300-a09b-58787e8a1b0c.png' />
+      <img className='logo' src='https://user-images.githubusercontent.com/10165959/155548924-be99c42a-30c3-4300-a09b-58787e8a1b0c.png' />
     </div>
     <AtomicSearchInterface engine={engine}>
       <div className='search'>
-        <AtomicSearchBox>
+        <AtomicSearchBox className='box'>
           <AtomicSearchBoxQuerySuggestions />
         </AtomicSearchBox>
       </div>
-      <AtomicFacetManager>
-        <AtomicFacet field='datepublished' /> 
-      </AtomicFacetManager>
+      <AtomicFacet field='languages'  label='Date Released' withSearch={false} /> 
       <br />
-      <AtomicResultList template={MyResultTemplateFunction} fieldsToInclude="description,id,title,link,year,datepublished,genres,duration,countries,languages,directors,budget,score,image" />
-    </ AtomicSearchInterface>
+      <AtomicResultList template={ResultTemplate} fieldsToInclude="description,id,title,link,year,datepublished,genres,duration,countries,languages,directors,budget,score,image" />
+    </AtomicSearchInterface>
     <p className="footer-heart">
       Made with <img className="emoji" alt="heart" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png" /> by <a href="https://samthomas.io/">Sam</a>
     </p>
